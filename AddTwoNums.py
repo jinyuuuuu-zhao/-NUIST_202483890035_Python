88c59b31086c9b812e5f68cf3963b7467ac5f865
jinyuuuuu-zhao <2577839182@qq.com>
2025-03-18 20:46:54 +0800

    AddTwoNums.py

--- AddTwoNums.py
+++ AddTwoNums.py
@@ -33,3 +33,32 @@ b = input("Second Number: ")
 res = add(a,b)
 print("The Answer is:")
 print(res)
+
+def factorial_while(num):
+    result = 1
+    while num > 0:
+        result = result * num
+        num = num - 1  
+    return result
+number = int(input("Please enter a number:"))
+print(factorial_while(number))
+
+def factorial_for(num):
+    result = 1
+    for i in range(1,num + 1):
+        result = result * i
+    return result
+
+number = int(input("Please enter a number:"))
+print(factorial_for(number))
+
+studentNames = ["Lisa","Liam","Leo","Linda"]
+for name in studentNames:
+    print(name + "Evans")
+
+new_name = input("Please add a name:")
+studentNames.append(new_name)
+
+for name in studentNames:
+    print(name + "Evans")
+
